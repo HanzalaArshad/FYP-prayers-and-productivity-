932b731005a5d698d85126dbece09590e110018c
HanzalaArshad <hanzalaarshad338@gmail.com>
2025-07-08 22:06:02 +0500

    feat: added ApiError and ApiResponse classes for unified error and response handling

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@
 import dotenv from  "dotenv"
 import { connect } from "mongoose"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
   path:'./env'
 })
 
 connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is Listening at port : ${PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.error("Express app encountered an error: ", error);
+      process.exit(1); 
+    });
+  })
+  .catch((err) => {
+    console.log('MONGODB CONNECTION FAILED !!!', err);
+  });
+
+
