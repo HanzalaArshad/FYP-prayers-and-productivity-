d16a3a3cba650b655688ff4641ded48da59dea34
HanzalaArshad <hanzalaarshad338@gmail.com>
2025-08-25 11:20:47 +0500

    `Added refresh token functionality, updated user controller and routes finally auth done`

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changePassword, getCurrentUser, login, logout, registerUser, UpdateAccountDetails } from "../controllers/user.controller.js";
+import { changePassword, getCurrentUser, lastActivity, login, logout, refreshAccessToken, registerUser, UpdateAccountDetails } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 
@@ -9,7 +9,12 @@ const userRouter=Router()
 
 userRouter.route("/register").post(registerUser)
 userRouter.route("/login").post(login)
+
 userRouter.route("/logout").post(verifyJwt,logout)
+userRouter.route("/refreshToken").post(refreshAccessToken)
+userRouter.use(verifyJwt, lastActivity);
+
+
 userRouter.route("/change-Password").post(verifyJwt,changePassword)
 userRouter.route("/my-account").get(verifyJwt,getCurrentUser)
 userRouter.route("/update-Account").post(verifyJwt,UpdateAccountDetails)
